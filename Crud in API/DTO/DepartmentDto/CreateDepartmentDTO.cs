2a86ff5b67cee1762a8056e90c5186432f144ff2
Osaid-Raddad <osaidislam1@gmail.com>
2025-07-04 22:42:07 +0300

    Validation Added

--- Crud in API/DTO/DepartmentDto/CreateDepartmentDTO.cs
+++ Crud in API/DTO/DepartmentDto/CreateDepartmentDTO.cs
@@ -1,10 +1,14 @@
 ﻿using Crud_in_API.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Crud_in_API.DTO.DepartmentDto
 {
     public class CreateDepartmentDTO
     {
         public int Id { get; set; }
+        [MinLength(5)]
+        [MaxLength(20)]
+        [Required]
         public string Name { get; set; }
         
     }
--- Crud in API/DTO/EmployeeDTO/CreateEmployeeDTO.cs
+++ Crud in API/DTO/EmployeeDTO/CreateEmployeeDTO.cs
@@ -1,10 +1,17 @@
-﻿namespace Crud_in_API.DTO.EmployeeDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud_in_API.DTO.EmployeeDTO
 {
     public class CreateEmployeeDTO
     {
         public int Id { get; set; }
+        [MinLength(4)]
+        [MaxLength(20)]
+        [Required]
         public string Name { get; set; }
+        [Range(0, 100000)]
         public decimal Salary { get; set; }
-        public int DepartmentId { get; set; }
+
+        public int? DepartmentId { get; set; }
     }
 }
